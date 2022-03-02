84d900b9eea72fde2f80ccb35044ecfcc9472bf8
lucas <lucas.santo@tallos.com.br>
2022-03-02 10:29:47 -0300

    update: Adicionando variaveis de ambientes e corrigindo arquivos docker

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./server');
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${3000}`);
+})
