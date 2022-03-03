3394e4fa8990db3404a16d9142454f37cac1ef9c
lucas <lucas.santo@tallos.com.br>
2022-03-03 10:08:39 -0300

    update: Criando  arquivos services e atulizando rotas e controllers

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const UsuariosControllers = require('./controllers/UsuarioControllers');
+const UsuarioController = new UsuariosControllers()
 
 const routes = express.Router();
 routes.get('/',(req, res)=>{
     return res.status(200).json("Salve");
 })
+routes.route('/usuarios')
+  .post((req, res) => {
+    return UsuarioController.createController(req, res)
+  })
+  
 module.exports = routes;
